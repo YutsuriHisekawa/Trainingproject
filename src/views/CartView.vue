f518ec05f1eab853d823bf9a03176cc47156bc13
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-03-04 10:27:46 +0700

    Add Timeline & Action Buttons on Cart Page

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -6,15 +6,30 @@ import Bottom from "@/components/Bottom.vue";
 <template>
   <Breadcrumb />
   <section class="relative">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-7">
-      <h2 class="title font-bold text-4xl leading-10 mb-8 text-black">
-        Keranjang
-      </h2>
+    <div class="max-w-7xl mx-auto px-4 pb-8 sm:px-6 lg:px-7">
+      <div
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 min-[550px]:gap-6 py-6"
+      >
+        <h2
+          class="flex items-center flex-col min-[550px]:flex-row gap-3 min-[550px]:gap-6 w-full title font-bold text-4xl leading-10 mb-8 text-black"
+        >
+          Keranjang
+        </h2>
+        <ul
+          class="steps steps-vertical md:steps-horizontal lg:steps-horizontal"
+        >
+          <li class="step step-primary">Keranjang</li>
+          <li class="step">Pengiriman</li>
+          <li class="step">Selesai</li>
+        </ul>
+      </div>
       <div class="grid grid-cols-1 lg:grid-cols-2 min-[550px]:gap-6 py-6">
         <div
           class="flex items-center flex-col min-[550px]:flex-row gap-3 min-[550px]:gap-6 w-full max-xl:justify-center max-xl:max-w-xl max-xl:mx-auto"
         >
-          <img src="../../public/img/cart-1.png" class="xl:w-[120px]" />
+          <div class="img-box">
+            <img src="../../img/cart-1.png" class="xl:w-[120px]" />
+          </div>
           <div class="pro-data w-full max-w-sm">
             <h5
               class="font-semibold text-xl leading-8 text-black max-[550px]:text-center"
@@ -48,6 +63,34 @@ import Bottom from "@/components/Bottom.vue";
           >
             Body
           </h6>
+          <div class="flex items-center gap-4">
+            <svg
+              width="44"
+              height="44"
+              viewBox="0 0 44 44"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="44" height="44" rx="5" fill="#04637D" />
+              <path
+                d="M31.3103 16.8784L27.1216 12.6887C26.9823 12.5494 26.8169 12.4389 26.6349 12.3635C26.4529 12.2881 26.2578 12.2493 26.0608 12.2493C25.8638 12.2493 25.6687 12.2881 25.4867 12.3635C25.3047 12.4389 25.1393 12.5494 25 12.6887L13.4397 24.25C13.2998 24.3888 13.1889 24.554 13.1134 24.736C13.0379 24.918 12.9994 25.1132 13 25.3103V29.5C13 29.8978 13.158 30.2793 13.4393 30.5606C13.7207 30.8419 14.1022 31 14.5 31H18.6897C18.8868 31.0006 19.082 30.9621 19.264 30.8866C19.446 30.8111 19.6112 30.7002 19.75 30.5603L31.3103 19C31.4496 18.8607 31.5602 18.6953 31.6356 18.5133C31.711 18.3313 31.7498 18.1362 31.7498 17.9392C31.7498 17.7422 31.711 17.5471 31.6356 17.3651C31.5602 17.1831 31.4496 17.0177 31.3103 16.8784ZM14.8103 25L22.75 17.0603L24.3147 18.625L16.375 26.5637L14.8103 25ZM14.5 26.8103L17.1897 29.5H14.5V26.8103ZM19 29.1897L17.4353 27.625L25.375 19.6853L26.9397 21.25L19 29.1897ZM28 20.1897L23.8103 16L26.0603 13.75L30.25 17.9387L28 20.1897Z"
+                fill="white"
+              />
+            </svg>
+            <svg
+              width="44"
+              height="44"
+              viewBox="0 0 44 44"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="44" height="44" rx="5" fill="#E2425C" />
+              <path
+                d="M19 13V14H14V16H15V29C15 29.5304 15.2107 30.0391 15.5858 30.4142C15.9609 30.7893 16.4696 31 17 31H27C27.5304 31 28.0391 30.7893 28.4142 30.4142C28.7893 30.0391 29 29.5304 29 29V16H30V14H25V13H19ZM17 16H27V29H17V16ZM19 18V27H21V18H19ZM23 18V27H25V18H23Z"
+                fill="white"
+              />
+            </svg>
+          </div>
         </div>
       </div>
       <div
@@ -57,7 +100,7 @@ import Bottom from "@/components/Bottom.vue";
           class="flex items-center flex-col min-[550px]:flex-row gap-3 min-[550px]:gap-6 w-full max-xl:justify-center max-xl:max-w-xl max-xl:mx-auto"
         >
           <div class="img-box">
-            <img src="../../public/img/cart-2.png" class="xl:w-[120px]" />
+            <img src="../../img/cart-2.png" class="xl:w-[120px]" />
           </div>
           <div class="pro-data w-full max-w-sm">
             <h5
@@ -92,6 +135,34 @@ import Bottom from "@/components/Bottom.vue";
           >
             Body
           </h6>
+          <div class="flex items-center gap-4">
+            <svg
+              width="44"
+              height="44"
+              viewBox="0 0 44 44"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="44" height="44" rx="5" fill="#04637D" />
+              <path
+                d="M31.3103 16.8784L27.1216 12.6887C26.9823 12.5494 26.8169 12.4389 26.6349 12.3635C26.4529 12.2881 26.2578 12.2493 26.0608 12.2493C25.8638 12.2493 25.6687 12.2881 25.4867 12.3635C25.3047 12.4389 25.1393 12.5494 25 12.6887L13.4397 24.25C13.2998 24.3888 13.1889 24.554 13.1134 24.736C13.0379 24.918 12.9994 25.1132 13 25.3103V29.5C13 29.8978 13.158 30.2793 13.4393 30.5606C13.7207 30.8419 14.1022 31 14.5 31H18.6897C18.8868 31.0006 19.082 30.9621 19.264 30.8866C19.446 30.8111 19.6112 30.7002 19.75 30.5603L31.3103 19C31.4496 18.8607 31.5602 18.6953 31.6356 18.5133C31.711 18.3313 31.7498 18.1362 31.7498 17.9392C31.7498 17.7422 31.711 17.5471 31.6356 17.3651C31.5602 17.1831 31.4496 17.0177 31.3103 16.8784ZM14.8103 25L22.75 17.0603L24.3147 18.625L16.375 26.5637L14.8103 25ZM14.5 26.8103L17.1897 29.5H14.5V26.8103ZM19 29.1897L17.4353 27.625L25.375 19.6853L26.9397 21.25L19 29.1897ZM28 20.1897L23.8103 16L26.0603 13.75L30.25 17.9387L28 20.1897Z"
+                fill="white"
+              />
+            </svg>
+            <svg
+              width="44"
+              height="44"
+              viewBox="0 0 44 44"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="44" height="44" rx="5" fill="#E2425C" />
+              <path
+                d="M19 13V14H14V16H15V29C15 29.5304 15.2107 30.0391 15.5858 30.4142C15.9609 30.7893 16.4696 31 17 31H27C27.5304 31 28.0391 30.7893 28.4142 30.4142C28.7893 30.0391 29 29.5304 29 29V16H30V14H25V13H19ZM17 16H27V29H17V16ZM19 18V27H21V18H19ZM23 18V27H25V18H23Z"
+                fill="white"
+              />
+            </svg>
+          </div>
         </div>
       </div>
       <div
@@ -101,7 +172,7 @@ import Bottom from "@/components/Bottom.vue";
           class="flex items-center flex-col min-[550px]:flex-row gap-3 min-[550px]:gap-6 w-full max-xl:justify-center max-xl:max-w-xl max-xl:mx-auto"
         >
           <div class="img-box">
-            <img src="../../public/img/cart-3.png" class="xl:w-[120px]" />
+            <img src="../../img/cart-3.png" class="xl:w-[120px]" />
           </div>
           <div class="pro-data w-full max-w-sm">
             <h5
@@ -136,6 +207,34 @@ import Bottom from "@/components/Bottom.vue";
           >
             Body
           </h6>
+          <div class="flex items-center gap-4">
+            <svg
+              width="44"
+              height="44"
+              viewBox="0 0 44 44"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="44" height="44" rx="5" fill="#04637D" />
+              <path
+                d="M31.3103 16.8784L27.1216 12.6887C26.9823 12.5494 26.8169 12.4389 26.6349 12.3635C26.4529 12.2881 26.2578 12.2493 26.0608 12.2493C25.8638 12.2493 25.6687 12.2881 25.4867 12.3635C25.3047 12.4389 25.1393 12.5494 25 12.6887L13.4397 24.25C13.2998 24.3888 13.1889 24.554 13.1134 24.736C13.0379 24.918 12.9994 25.1132 13 25.3103V29.5C13 29.8978 13.158 30.2793 13.4393 30.5606C13.7207 30.8419 14.1022 31 14.5 31H18.6897C18.8868 31.0006 19.082 30.9621 19.264 30.8866C19.446 30.8111 19.6112 30.7002 19.75 30.5603L31.3103 19C31.4496 18.8607 31.5602 18.6953 31.6356 18.5133C31.711 18.3313 31.7498 18.1362 31.7498 17.9392C31.7498 17.7422 31.711 17.5471 31.6356 17.3651C31.5602 17.1831 31.4496 17.0177 31.3103 16.8784ZM14.8103 25L22.75 17.0603L24.3147 18.625L16.375 26.5637L14.8103 25ZM14.5 26.8103L17.1897 29.5H14.5V26.8103ZM19 29.1897L17.4353 27.625L25.375 19.6853L26.9397 21.25L19 29.1897ZM28 20.1897L23.8103 16L26.0603 13.75L30.25 17.9387L28 20.1897Z"
+                fill="white"
+              />
+            </svg>
+            <svg
+              width="44"
+              height="44"
+              viewBox="0 0 44 44"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="44" height="44" rx="5" fill="#E2425C" />
+              <path
+                d="M19 13V14H14V16H15V29C15 29.5304 15.2107 30.0391 15.5858 30.4142C15.9609 30.7893 16.4696 31 17 31H27C27.5304 31 28.0391 30.7893 28.4142 30.4142C28.7893 30.0391 29 29.5304 29 29V16H30V14H25V13H19ZM17 16H27V29H17V16ZM19 18V27H21V18H19ZM23 18V27H25V18H23Z"
+                fill="white"
+              />
+            </svg>
+          </div>
         </div>
       </div>
       <div
@@ -145,7 +244,7 @@ import Bottom from "@/components/Bottom.vue";
           class="flex items-center flex-col min-[550px]:flex-row gap-3 min-[550px]:gap-6 w-full max-xl:justify-center max-xl:max-w-xl max-xl:mx-auto"
         >
           <div class="img-box">
-            <img src="../../public/img/cart-4.png" class="xl:w-[120px]" />
+            <img src="../../img/cart-4.png" class="xl:w-[120px]" />
           </div>
           <div class="pro-data w-full max-w-sm">
             <h5
@@ -180,6 +279,34 @@ import Bottom from "@/components/Bottom.vue";
           >
             Body
           </h6>
+          <div class="flex items-center gap-4">
+            <svg
+              width="44"
+              height="44"
+              viewBox="0 0 44 44"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="44" height="44" rx="5" fill="#04637D" />
+              <path
+                d="M31.3103 16.8784L27.1216 12.6887C26.9823 12.5494 26.8169 12.4389 26.6349 12.3635C26.4529 12.2881 26.2578 12.2493 26.0608 12.2493C25.8638 12.2493 25.6687 12.2881 25.4867 12.3635C25.3047 12.4389 25.1393 12.5494 25 12.6887L13.4397 24.25C13.2998 24.3888 13.1889 24.554 13.1134 24.736C13.0379 24.918 12.9994 25.1132 13 25.3103V29.5C13 29.8978 13.158 30.2793 13.4393 30.5606C13.7207 30.8419 14.1022 31 14.5 31H18.6897C18.8868 31.0006 19.082 30.9621 19.264 30.8866C19.446 30.8111 19.6112 30.7002 19.75 30.5603L31.3103 19C31.4496 18.8607 31.5602 18.6953 31.6356 18.5133C31.711 18.3313 31.7498 18.1362 31.7498 17.9392C31.7498 17.7422 31.711 17.5471 31.6356 17.3651C31.5602 17.1831 31.4496 17.0177 31.3103 16.8784ZM14.8103 25L22.75 17.0603L24.3147 18.625L16.375 26.5637L14.8103 25ZM14.5 26.8103L17.1897 29.5H14.5V26.8103ZM19 29.1897L17.4353 27.625L25.375 19.6853L26.9397 21.25L19 29.1897ZM28 20.1897L23.8103 16L26.0603 13.75L30.25 17.9387L28 20.1897Z"
+                fill="white"
+              />
+            </svg>
+            <svg
+              width="44"
+              height="44"
+              viewBox="0 0 44 44"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="44" height="44" rx="5" fill="#E2425C" />
+              <path
+                d="M19 13V14H14V16H15V29C15 29.5304 15.2107 30.0391 15.5858 30.4142C15.9609 30.7893 16.4696 31 17 31H27C27.5304 31 28.0391 30.7893 28.4142 30.4142C28.7893 30.0391 29 29.5304 29 29V16H30V14H25V13H19ZM17 16H27V29H17V16ZM19 18V27H21V18H19ZM23 18V27H25V18H23Z"
+                fill="white"
+              />
+            </svg>
+          </div>
         </div>
       </div>
       <div class="flex justify-center border-t border-gray-300 py-14">
