2f6387cba830a7c9e4c8010194beebdc6e7e3f66
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-03-04 14:42:41 +0700

    Resources Page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import ContactView from "../views/ContactView.vue";
 import FaqView from "../views/FaqView.vue";
 import CareerView from "../views/CareerView.vue";
 import CartView from "../views/CartView.vue";
+import ResourceView from "../views/ResourceView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,6 +39,11 @@ const router = createRouter({
       path: "/cart",
       name: "cart",
       component: CartView,
+    },
+    {
+      path: "/resource",
+      name: "resource",
+      component: ResourceView,
     },
   ],
 });
